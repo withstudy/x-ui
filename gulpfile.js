e4e44783ec624f6b8654ea90a725c289e6936ccf
xhb <1484389028@qq.com>
2023-09-05 21:12:25 +0800

    feat: css使用gulp打包，方便按需引入

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const { src, dest, series } = require('gulp')
+
+const sass = require('gulp-sass')(require('sass'))
+
+const sassGlob = require('gulp-sass-glob')
+
+const postcss = require('gulp-postcss')
+
+const autoprefixer = require('autoprefixer')
+
+const cssnano = require('cssnano')
+
+const plugins = [
+    autoprefixer(),
+    cssnano()
+]
+
+function scss() {
+    return src('./src/styles/*.scss')
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(postcss(plugins))
+    .pipe(dest('lib/styles'))
+}
+
+exports.default = series(scss)
